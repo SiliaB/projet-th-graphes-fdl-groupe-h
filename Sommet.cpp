e9341da962459183b686015d86d0770824f6d3c1
Faustine Galichet <faustine.galichet@edu.ece.fr>
2020-04-20 16:47:26 +0200

    modif sommet

--- Sommet.cpp
+++ Sommet.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Sommet.h"
+
+Sommet::Sommet(int num) : m_numero{num} {}
+
+
+void Sommet::addSuccesseur(const Sommet* succ) {
+    m_successeurs.push_back(succ);
+}
+
+const std::vector<const Sommet*>& Sommet::getSuccesseurs() const {
+    return m_successeurs;
+}
+
+int Sommet::getNumero() const {
+    return m_numero;
+}
+
+
+int Sommet::setNumero(int newNumero) {
+    return m_numero=newNumero;
+}
+
+void Sommet::afficher() const {
+    std::cout << "   Sommet " << m_numero << " : ";
+    for(auto addrSommet : m_successeurs) {
+        std::cout << addrSommet->getNumero() << " ";
+    }
+}
